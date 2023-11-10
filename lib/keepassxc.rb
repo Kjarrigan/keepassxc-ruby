49e0df6e5637851cc95c49efca6d2f9a9f79113f
Holger Arndt <holger.arndt@hetzner.com>
2023-11-10 10:20:38 +0100

    fghjk

--- lib/keepassxc.rb
+++ lib/keepassxc.rb
@@ -13,8 +13,11 @@ module KeepassXC
   autoload :VERSION, 'keepassxc/version'
 
   def self.open_session(client_name)
+    raise ArgumentError if client_name.nil?
+
+    client_name = client_name.to_s
     cfg = KeyStore.find_or_create
-    kpx = Client.new client_client_name: client_name
+    kpx = Client.new client_name: client_name
     kpx.change_public_keys
     if cfg.profiles[client_name]
       kpx.client_identifier = cfg.profiles[client_name]
