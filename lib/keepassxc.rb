f634644ba12c08d9414811b5fe253fd0f2988aa5
Holger Arndt <holger.arndt@hetzner.com>
2023-11-10 10:15:17 +0100

    Feat: Convert exe into a re-usable wrapper

--- lib/keepassxc.rb
+++ lib/keepassxc.rb
@@ -11,4 +11,21 @@ module KeepassXC
   autoload :Helper, 'keepassxc/helper'
   autoload :KeyStore, 'keepassxc/key_store'
   autoload :VERSION, 'keepassxc/version'
+
+  def self.open_session(client_name)
+    cfg = KeyStore.find_or_create
+    kpx = Client.new client_client_name: client_name
+    kpx.change_public_keys
+    if cfg.profiles[client_name]
+      kpx.client_identifier = cfg.profiles[client_name]
+    else
+      kpx.associate
+      cfg.profiles[client_name] = kpx.client_identifier
+      cfg.save
+    end
+    kpx.test_associate
+    return yield(kpx) if block_given?
+
+    kpx
+  end
 end
